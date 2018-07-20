01e0fa62ac008696966140951a78db62ca217495
The App Master <tam@mdm.fibertel.com.ar>
2018-07-20 13:23:15 -0300

    hotfix validator json schema

--- generators/app/templates/helpers/validate.js
+++ generators/app/templates/helpers/validate.js
@@ -1,23 +1,13 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
 const Ajv = require('ajv')
-var ajv = new Ajv({
-  allErrors: true,
-  jsonPointers: true
-})
 
+var ajv = new Ajv({ allErrors: true, jsonPointers: true })
+const errors = err => ({ error: err.keyword, path: err.dataPath || '/', message: err.message })
 module.exports = (payload, schemaPath) => {
   let schema = yaml.safeLoad(fs.readFileSync(schemaPath, 'utf8'))
   return {
     valid: ajv.validate(schema, payload),
-    errors: ajv.errors
-      ? ajv.errors.map(err => (
-        {
-          error: err.keyword,
-          path: err.dataPath || '/',
-          message: err.message
-        }
-      ))
-      : null
+    errors: ajv.errors ? ajv.errors.map(errors) : null
   }
 }
