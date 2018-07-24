19db86364cd91f1588a94b793d5fbdacb9be2b0a
Frangeris Peguero <frangeris.20@gmail.com>
2018-07-24 09:17:41 -0300

    Refactor validator to return json schema errors

--- generators/app/templates/helpers/validate.js
+++ generators/app/templates/helpers/validate.js
@@ -1,13 +1,10 @@
-const yaml = require('js-yaml')
 const fs = require('fs')
 const Ajv = require('ajv')
 
 var ajv = new Ajv({ allErrors: true, jsonPointers: true })
-const errors = err => ({ error: err.keyword, path: err.dataPath || '/', message: err.message })
-module.exports = (payload, schemaPath) => {
-  let schema = yaml.safeLoad(fs.readFileSync(schemaPath, 'utf8'))
-  return {
-    valid: ajv.validate(schema, payload),
-    errors: ajv.errors ? ajv.errors.map(errors) : null
+module.exports = (payload, path) => {
+  const schema = JSON.parse(fs.readFileSync(path, 'utf8'))
+  if (!ajv.validate(schema, payload)) {
+    throw ajv.errors.map(err => ({ error: err.keyword, path: err.dataPath || '/', message: err.message }))
   }
 }
